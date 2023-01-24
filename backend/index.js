4bd2fb991e5286b1b5c4771f6cbd07b1d6d01a9d
Leonard von Wolcker <leonard@plug-n-trade.com>
2023-01-24 09:14:07 +0100

    blabla

--- backend/index.js
+++ backend/index.js
@@ -7,44 +7,46 @@ const crypto = require("crypto");
 const Joi = require("joi");
 const app = express();
 
-//urls is a static array as assignment didn't give promission to use DB
-const urls = [
+let urls = [
   {
-    id: "f9b327e70bbcf42494ccb28b2d98e00e",
-    value: "www.google.com",
+    id: 1,
+    value: "https://www.google.com",
     description: "google",
-    maxSearched: 10,
+    maxSearched: 2,
   },
   {
-    id: "f9b327e70bbcf42494ccb28b2d98e00f",
-    value: "www.facebook.com",
+    id: 2,
+    value: "https://www.facebook.com",
     description: "fb",
     maxSearched: 10,
   },
   {
-    id: "f9b327e70bbcf42494ccb28b2d98e00g",
-    value: "www.netflix.com",
+    id: 3,
+    value: "https://www.netflix.com",
     description: "netflix",
     maxSearched: 10,
   },
 ];
 
-//Mades all "req.body" into json
 app.use(express.json());
 
-//a must have hello world
 app.get("/", (req, res) => {
   return res.send("Hello World");
 });
 
 //GET to get the specific url that uses a description
 app.get("/api/url/:description", (req, res) => {
-  const url = getUrl(req.params.description);
-  if (!url)
-    return res
-      .status(404)
-      .send(`the url description: ${req.params.description} was not found`);
-  return res.send(url);
+  const description = req.params.description;
+  const url = getUrl(description);
+
+  if (!url) return res.status(404).send(`the url description: ${description} was not found`);
+
+  if (!url.maxSearched) return res.status(404).send(`the url have been used to many times`);
+
+  url.maxSearched = url.maxSearched--;
+
+  const website = url.value;
+  return res.redirect(website);
 });
 
 //POSTs a url to the urls array, note that new urls will disappear when app is restarted
@@ -69,16 +71,11 @@ app.put("/api/url:id", (req, res) => {
   const { error } = schema.validate(req.body);
   if (error) return res.status(400).send(error);
   const url = getUrl(req.params.description);
-  if (!url)
-    return res
-      .status(404)
-      .send(`the url description: ${req.params.description} was not found`);
+  if (!url) return res.status(404).send(`the url description: ${req.params.description} was not found`);
 
   url.value = req.body.value != null ? req.body.value : url.value;
-  url.description =
-    req.body.description != null ? req.body.description : url.description;
-  url.maxSearched =
-    req.body.maxSearched != null ? req.body.maxSearched : url.maxSearched;
+  url.description = req.body.description != null ? req.body.description : url.description;
+  url.maxSearched = req.body.maxSearched != null ? req.body.maxSearched : url.maxSearched;
   return res.send(url);
 });
 
@@ -93,7 +90,7 @@ function getJoiSchema() {
 }
 
 function getUrl(description) {
-  return urls.filter((u) => u.description == description);
+  return urls.find((u) => u.description == description);
 }
 
 function getNewId() {
