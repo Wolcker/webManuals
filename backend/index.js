22816564390c4785a6c5f3290db20493761297fc
Leonard von Wolcker <leonard@plug-n-trade.com>
2023-01-24 13:34:31 +0100

    GET, PUT, POST works

--- backend/index.js
+++ backend/index.js
@@ -9,41 +9,55 @@ const app = express();
 
 let urls = [
   {
-    id: 1,
+    id: "d424b1b7bcc45860ec47f09c219861da",
     value: "https://www.google.com",
     description: "google",
     maxSearched: 2,
+    searched: 0,
+    maxTime: addMinutes(new Date(), 5),
   },
   {
-    id: 2,
+    id: "d424b1b7bcc45860ec47f09c219861dc",
     value: "https://www.facebook.com",
     description: "fb",
-    maxSearched: 10,
+    maxSearched: 5,
+    searched: 0,
+    maxTime: addMinutes(new Date(), 1),
   },
   {
-    id: 3,
+    id: "d424b1b7bcc45860ec47f09c219861dj",
     value: "https://www.netflix.com",
     description: "netflix",
-    maxSearched: 10,
+    maxSearched: 7,
+    searched: 0,
+    maxTime: addMinutes(new Date(), 3),
   },
 ];
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.send("Hello World");
+app.get("/api/url", (req, res) => {
+  return res.send(urls);
 });
 
 //GET to get the specific url that uses a description
 app.get("/api/url/:description", (req, res) => {
   const description = req.params.description;
-  const url = getUrl(description);
+  const url = getUrlByDescription(description);
 
-  if (!url) return res.status(404).send(`the url description: ${description} was not found`);
+  if (!url) {
+    return res.status(404).send(`The url description: ${description} was not found`);
+  }
 
-  if (!url.maxSearched) return res.status(404).send(`the url have been used to many times`);
+  if (url.searched >= url.maxSearched) {
+    return res.status(404).send(`The url have been used to many times`);
+  }
 
-  url.maxSearched = url.maxSearched--;
+  if (new Date() > url.maxTime) {
+    return res.status(404).send(`Sorry to late, the time have expired to use this short url`);
+  }
+
+  url.searched++;
 
   const website = url.value;
   return res.redirect(website);
@@ -53,29 +67,57 @@ app.get("/api/url/:description", (req, res) => {
 app.post("/api/url", (req, res) => {
   const schema = getJoiSchema();
   const { error } = schema.validate(req.body);
-  if (error) return res.status(400).send(error);
+  if (error) {
+    return res.status(400).send(error);
+  }
+  const { value, description, maxSearched, maxTime } = req.body;
+
+  const tmpUrl = getUrlByValue(value);
+
+  if (tmpUrl) {
+    return res.status(400).send(error);
+  }
 
   const url = {
     id: getNewId(),
-    value: req.body.value,
-    description: req.body.description,
-    maxSearched: req.body.maxSearched,
+    value: value,
+    description: description,
+    maxSearched: maxSearched,
+    searched: 0,
+    maxTime: addMinutes(new Date(), maxTime != null ? maxTime : 1),
   };
+
   urls.push(url);
   return res.send(urls);
 });
 
 //Will update the specific url
-app.put("/api/url:id", (req, res) => {
+app.put("/api/url/:id", (req, res) => {
   const schema = getJoiSchema();
   const { error } = schema.validate(req.body);
-  if (error) return res.status(400).send(error);
-  const url = getUrl(req.params.description);
-  if (!url) return res.status(404).send(`the url description: ${req.params.description} was not found`);
+  const { value, description, maxSearched, searched, maxTime } = req.body;
+  const url = getUrlById(req.params.id);
+  const urlDescription = getUrlByDescription(description);
+  const urlValue = getUrlByValue(value);
 
-  url.value = req.body.value != null ? req.body.value : url.value;
-  url.description = req.body.description != null ? req.body.description : url.description;
-  url.maxSearched = req.body.maxSearched != null ? req.body.maxSearched : url.maxSearched;
+  if (error) {
+    return res.status(400).send(error);
+  }
+
+  if (!url) {
+    return res.status(404).send(`the url id: ${req.params.id} was not found`);
+  }
+
+  if (urlDescription || urlValue) {
+    return res.status(400).send(`Value or description are already used`);
+  }
+
+  url.value = value != null ? value : url.value;
+  url.description = description != null ? description : url.description;
+  url.maxSearched = maxSearched != null ? maxSearched : url.maxSearched;
+  url.searched = searched != 0 ? searched : url.searched;
+  url.maxTime = maxTime != 0 ? maxTime : url.maxTime;
+
   return res.send(url);
 });
 
@@ -86,11 +128,21 @@ function getJoiSchema() {
     value: Joi.string().required().min(2).max(1000),
     description: Joi.string().required().min(2),
     maxSearched: Joi.number().required().min(0).max(20),
+    searched: Joi.number().optional().min(0),
+    maxTime: Joi.number().optional().min(0),
   });
 }
 
-function getUrl(description) {
+function getUrlByDescription(description) {
   return urls.find((u) => u.description == description);
+}
+
+function getUrlByValue(value) {
+  return urls.find((u) => u.value == value);
+}
+
+function getUrlById(id) {
+  return urls.find((u) => u.id == id);
 }
 
 function getNewId() {
@@ -101,4 +153,15 @@ function getNewId() {
     checkId = urls.find((u) => u.id == id);
   }
   return id;
+}
+
+function addMinutes(date, minutes) {
+  return new Date(date.getTime() + minutes * 60000).toLocaleDateString("sv-SE", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
 }
