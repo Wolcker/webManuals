338704d8cd4b1a019377cf96ccf754b993d6eacd
Leonard von Wolcker <leonard@plug-n-trade.com>
2023-01-25 14:12:32 +0100

    blabla

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div>hej</div>;
+  }
+}
+
+export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+
+//     </div>
+//   );
+// }
+
+// export default App;
