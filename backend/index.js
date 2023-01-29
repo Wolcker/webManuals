a9889d89d5dc2b9e08eea3c3b5cc0fc2517b8138
Wolcker <leonard_von_wolcker@hotmail.com>
2023-01-29 07:53:33 +0100

    working endresult

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,27 @@
-//Backend documentation:
-//Comments will say what the code does if not given a smart name
-//Comments will be used before the code is executed
-
 const express = require("express");
 const crypto = require("crypto");
 const cors = require("cors");
 const Joi = require("joi");
 const app = express();
+const port = 5000;
 
-let urls = [
+let URLS = [
   {
     id: "d424b1b7bcc45860ec47f09c219861da",
     value: "https://www.google.com",
-    description: "google",
-    maxSearched: 2,
-    searched: 0,
-    maxTime: addMinutes(new Date(), 5),
+    redirectURL: "google",
+    maxTime: addMinutes(new Date(), 0),
   },
   {
     id: "d424b1b7bcc45860ec47f09c219861dc",
     value: "https://www.facebook.com",
-    description: "fb",
-    maxSearched: 5,
-    searched: 0,
+    redirectURL: "fb",
     maxTime: addMinutes(new Date(), 1),
   },
   {
     id: "d424b1b7bcc45860ec47f09c219861dj",
     value: "https://www.netflix.com",
-    description: "netflix",
-    maxSearched: 7,
-    searched: 0,
+    redirectURL: "netflix",
     maxTime: addMinutes(new Date(), 3),
   },
 ];
@@ -38,125 +29,94 @@ let urls = [
 app.use(express.json());
 app.use(cors());
 app.get("/api/url", (req, res) => {
-  return res.send(urls);
+  return res.send(URLS);
 });
 
 //GET to get the specific url that uses a description
-app.get("/api/url/:description", (req, res) => {
-  const description = req.params.description;
-  const url = getUrlByDescription(description);
+app.get("/api/url/:redirectURL", (req, res) => {
+  const redirectURL = req.params.redirectURL;
+  const url = getUrlByshortner(redirectURL);
+  const { value: website, maxTime } = url;
 
   if (!url) {
-    return res.status(404).send(`The url description: ${description} was not found`);
+    return res.status(404).send(`The url redirectURL: ${redirectURL} was not found`);
   }
-
-  if (url.searched >= url.maxSearched) {
-    return res.status(404).send(`The url have been used to many times`);
-  }
-
-  if (new Date() > url.maxTime) {
+  if (new Date() > maxTime && maxTime != 0) {
     return res.status(404).send(`Sorry to late, the time have expired to use this short url`);
   }
 
-  url.searched++;
-
-  const website = url.value;
-  return res.redirect(website);
+  res.redirect(website);
 });
 
 //POSTs a url to the urls array, note that new urls will disappear when app is restarted
 app.post("/api/url", (req, res) => {
   const schema = getJoiSchema();
-  const { error } = schema.validate(req.body);
-  if (error) {
-    return res.status(400).send(error);
+
+  const result = schema.validate(req.body);
+  const { error } = result;
+  const valid = error == null;
+
+  if (!valid) {
+    res.status(422).json({
+      message: "Invalid request",
+      data: body,
+    });
+  } else {
+    const { url, maxTime } = req.body;
+    const website = getWebsite(url);
+
+    const tmpUrl = {
+      id: getNewId(),
+      value: website,
+      redirectURL: createShortner(),
+      infinitSearch: false,
+      maxTime: addMinutes(new Date(), maxTime),
+    };
+
+    URLS.push(tmpUrl);
+    res.status(200).send(tmpUrl);
   }
-  const { value, description, maxSearched, maxTime } = req.body;
-
-  const tmpUrl = getUrlByValue(value);
-
-  if (tmpUrl) {
-    return res.status(400).send(error);
-  }
-
-  const url = {
-    id: getNewId(),
-    value: value,
-    description: description,
-    maxSearched: maxSearched,
-    searched: 0,
-    maxTime: addMinutes(new Date(), maxTime != null ? maxTime : 1),
-  };
-
-  urls.push(url);
-  return res.send(urls);
 });
 
-//Will update the specific url
-app.put("/api/url/:id", (req, res) => {
-  const schema = getJoiSchema();
-  const { error } = schema.validate(req.body);
-  const { value, description, maxSearched, searched, maxTime } = req.body;
-  const url = getUrlById(req.params.id);
-  const urlDescription = getUrlByDescription(description);
-  const urlValue = getUrlByValue(value);
-
-  if (error) {
-    return res.status(400).send(error);
-  }
-
-  if (!url) {
-    return res.status(404).send(`the url id: ${req.params.id} was not found`);
-  }
-
-  if (urlDescription || urlValue) {
-    return res.status(400).send(`Value or description are already used`);
-  }
-
-  url.value = value != null ? value : url.value;
-  url.description = description != null ? description : url.description;
-  url.maxSearched = maxSearched != null ? maxSearched : url.maxSearched;
-  url.searched = searched != 0 ? searched : url.searched;
-  url.maxTime = maxTime != 0 ? maxTime : url.maxTime;
-
-  return res.send(url);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
 
-app.listen(5000, () => console.log("Listening on port 5000"));
-
+//Here comes functions
+// Joi schema to validate
 function getJoiSchema() {
-  return Joi.object({
-    value: Joi.string().required().min(2).max(1000),
-    description: Joi.string().required().min(2),
-    maxSearched: Joi.number().required().min(0).max(20),
-    searched: Joi.number().optional().min(0),
+  return Joi.object().keys({
+    url: Joi.string().required().min(8).max(1000),
     maxTime: Joi.number().optional().min(0),
   });
 }
 
-function getUrlByDescription(description) {
-  return urls.find((u) => u.description == description);
-}
-
-function getUrlByValue(value) {
-  return urls.find((u) => u.value == value);
-}
-
-function getUrlById(id) {
-  return urls.find((u) => u.id == id);
-}
-
+//functions for the creation of tmpUrl, when POST.
 function getNewId() {
   let id = crypto.randomBytes(16).toString("hex");
-  let checkId = urls.find((u) => u.id == id);
+  let checkId = URLS.find((u) => u.id == id);
   while (checkId) {
     id = crypto.randomBytes(16).toString("hex");
-    checkId = urls.find((u) => u.id == id);
+    checkId = URLS.find((u) => u.id == id);
   }
   return id;
 }
 
+function getWebsite(value) {
+  if (value.startsWith("http://") || value.startsWith("https://")) {
+    return value;
+  }
+  return `https://${value}`;
+}
+
+function createShortner() {
+  return Math.random().toString(36).substring(2, 7);
+}
+
 function addMinutes(date, minutes) {
+  if (!minutes) {
+    return 0;
+  }
   return new Date(date.getTime() + minutes * 60000).toLocaleDateString("sv-SE", {
     year: "numeric",
     month: "numeric",
@@ -165,4 +125,10 @@ function addMinutes(date, minutes) {
     minute: "2-digit",
     second: "2-digit",
   });
+}
+
+//functions for searching Urls when redirecting in GET and specifik shortner/redirectURL
+function getUrlByshortner(redirectURL) {
+  const url = URLS.find((u) => u.redirectURL === redirectURL);
+  return url;
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,67 +1,118 @@
 import React, { Component } from "react";
+import axios from "axios";
+import Joi from "joi";
+
+const URL = "http://localhost:5000/api/url/";
 
 class App extends Component {
-  handleGenerateUrl = () => {
-    console.log("kebabsås");
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: "",
+      redirectURL: "",
+      maxTime: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleGenerate = async (event) => {
+    event.preventDefault();
+    try {
+      const { url, maxTime } = this.state;
+
+      if (!checkValidMaxTime(maxTime)) {
+        this.setState({ maxTime: 0 });
+      }
+
+      if (!checkValidUrl(url)) {
+      }
+
+      const response = await axios.post(URL, {
+        url: url,
+        maxTime: maxTime,
+      });
+      const { value, redirectURL } = response.data;
+      this.setState({
+        url: value,
+        redirectURL: redirectURL,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  handleGoTo = async (event) => {
+    event.preventDefault();
+    window.open(this.state.url, "_blank");
   };
 
   render() {
     return (
-      <>
-        <img src="/logo.png" alt="logo" className="center" />
-        {getTable()};
-      </>
+      <form className="center">
+        <label>URL shortner</label>
+        <br />
+        {inputGenerator("url", this.state.url, "www.wolcker.com", this.handleChange)}
+        {inputGenerator("maxTime", this.state.maxTime, "Max time (optional)", this.handleChange)}
+        {buttonGenerator("Generate", this.handleGenerate)}
+        <br />
+        <br />
+        {inputGenerator("redirectURL", this.state.redirectURL, "Redirect URL", this.handleChange)}
+        {buttonGenerator("Go to", this.handleGoTo)}
+      </form>
     );
   }
 }
 
-function getTable() {
+// below is the creation of input and button
+const inputGenerator = (name, value, placeholder, handleChange) => {
   return (
-    <table className="center">
-      {getTableHeader()}
-      {getTableBody()}
-    </table>
+    <input
+      type="text"
+      name={name}
+      value={value}
+      onChange={handleChange}
+      placeholder={placeholder}
+    />
   );
-}
+};
 
-function getTableHeader() {
+const buttonGenerator = (placeholder, onClick) => {
   return (
-    <thead>
-      <tr>
-        <th colSpan="2">URL shortner</th>
-      </tr>
-    </thead>
-  );
-}
-
-function getTableBody() {
-  return (
-    <tbody>
-      <tr>
-        <td>{getInput("urlInput", "text", "www.google.com")}</td>
-        <td>{getInput("descriptionInput", "text", "Enter shortner here")}</td>
-      </tr>
-      <tr>
-        <td>{getInput("maxSearchInput", "text", "maxSearch")}</td>
-        <td>{getInput("maxTimeInput", "text", "minutes")}</td>
-      </tr>
-      <tr>
-        <td>{getButton("urlInputButton", "Generate")}</td>
-      </tr>
-    </tbody>
-  );
-}
-
-function getInput(id, type, label) {
-  return <input type={type} id={id} value={label} />;
-}
-
-function getButton(id, label) {
-  return (
-    <button onChange={() => this.handleGenerateUrl()} id={id}>
-      {label}
+    <button type="submit" onClick={onClick}>
+      {placeholder}
     </button>
   );
+};
+
+function checkValidMaxTime(maxTime) {
+  const schema = Joi.object().keys({
+    maxTime: Joi.number().required().min(1),
+  });
+  const result = schema.validate(maxTime);
+  const { error } = result;
+  const valid = error == null;
+
+  if (!valid) {
+    return false;
+  }
+
+  return true;
+}
+
+function checkValidUrl(str) {
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
+  return !!pattern.test(str);
 }
 
 export default App;
